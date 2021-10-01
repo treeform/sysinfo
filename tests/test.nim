6974c3d9ca461db25f3b4d0028a05931dd561fc9
treeform <starplant@gmail.com>
2021-09-30 17:32:27 -0700

    Fix tests

---
+++ tests/test.nim
@@ -0,0 +1,19 @@
+import ../src/sysinfo
+
+echo "getMachineGuid: ", getMachineGuid()
+echo "getMachineModel: ", getMachineModel()
+echo "getMachineName: ", getMachineName()
+echo "getMachineManufacturer: ", getMachineManufacturer()
+echo "getOsName: ", getOsName()
+echo "getOsVersion: ", getOsVersion()
+echo "getOsSerialNumber: ", getOsSerialNumber()
+echo "getCpuName: ", getCpuName()
+echo "getCpuGhz: ", getCpuGhz(), "GHz"
+echo "getCpuManufacturer: ", getCpuManufacturer()
+echo "getNumCpus: ", getNumCpus()
+echo "getNumTotalCores: ", getNumTotalCores()
+echo "getTotalMemory: ", getTotalMemory().float / 1024 / 1024 / 1024, "GB"
+echo "getFreeMemory: ", getFreeMemory().float / 1024 / 1024 / 1024, "GB"
+echo "getGpuName: ", getGPUName()
+echo "getGpuDriverVersion: ", getGpuDriverVersion()
+echo "getGpuMaxFPS: ", getGpuMaxFPS()
